2d66f5c8089520876bf92113e73860d2be8d7adf
Seth <8thseth@gmail.com>
2023-05-30 13:28:24 -0300

    Updates

--- blocks/src/services/api.ts
+++
@@ -1,5 +0,0 @@
-export async function getFamilies(skip: number, take:number) {
-  const response = await fetch(`https://test-candidaturas-front-end.onrender.com/families?skip=${skip}&take=${take}`);
-  const jsonData = await response.json();
-  return jsonData;
-}
